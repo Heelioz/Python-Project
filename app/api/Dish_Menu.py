a97c07570d84c8f789d6650d9bf0ed665398ba2c
Heelioz <115253511+Heelioz@users.noreply.github.com>
2024-07-03 01:32:30 -0400

    Menu CRUD

--- app/api/Dish_Menu.py
+++ app/api/Dish_Menu.py
@@ -22,15 +22,17 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+#GESTION PLATILLOS
+
 @router.post("/registrar_platillo")
-async def create_ingredient(platillo: Platillos, db:db_dependency):
+async def create_dish(platillo: Platillos, db:db_dependency):
     db_dish = orm_model.Platillos(fullname= platillo.fullname, MetodoPreparacion= platillo.MetodoPreparacion, Ingredientes= platillo.Ingredientes, Menu= platillo.Menu, Precio=platillo.Precio)
     db.add(db_dish)
     db.commit()
 
 
 @router.put("/modificar_platillo/{nombre}")
-async def update_ingredient_by_name(nombre: str, platillo: Platillos, db:db_dependency):
+async def update_dish_by_name(nombre: str, platillo: Platillos, db:db_dependency):
     db_dish = db.query(orm_model.Platillos).filter(orm_model.Platillos.fullname == nombre).first()
 
     if not  db_dish:
@@ -46,7 +48,7 @@ async def update_ingredient_by_name(nombre: str, platillo: Platillos, db:db_depe
     return {"message": "Platillo actualizado exitosamente"}
 
 @router.delete("/eliminar_platillo/{nombre}")
-async def delete_ingredient_by_name(nombre: str, db: db_dependency):
+async def delete_dish_by_name(nombre: str, db: db_dependency):
     db_dish = db.query(orm_model.Platillos).filter(orm_model.Platillos.fullname == nombre).first()
 
     if not db_dish:
@@ -55,4 +57,37 @@ async def delete_ingredient_by_name(nombre: str, db: db_dependency):
     db.delete(db_dish)
     db.commit()
     return {"message": "Platillo eliminado exitosamente"}
+
+#GESTION MENUS
+
+@router.post("/registrar_menu")
+async def create_menu(menu: Menu, db:db_dependency):
+    db_menu = orm_model.Menu(fullname= menu.fullname, Platos= menu.Platos)
+    db.add(db_menu)
+    db.commit()
+
+@router.put("/modificar_menu/{nombre}")
+async def update_menu_by_name(nombre: str, menu: Menu, db:db_dependency):
+    db_menu = db.query(orm_model.Menu).filter(orm_model.Menu.fullname == nombre).first()
+
+    if not  db_menu:
+        return {"message": "Menu no encontrado"}, 404
+
+    db_menu.fullname = menu.fullname
+    db_menu.Platos = menu.Platos
+   
+
+    db.commit()
+    return {"message": "Menu actualizado exitosamente"}
+
+@router.delete("/eliminar_menu/{nombre}")
+async def delete_menu_by_name(nombre: str, db: db_dependency):
+    db_menu = db.query(orm_model.Menu).filter(orm_model.Menu.fullname == nombre).first()
+
+    if not db_menu:
+        return {"message": "Menu no encontrado"}, 404
+
+    db.delete(db_menu)
+    db.commit()
+    return {"message": "Menu eliminado exitosamente"}
 
